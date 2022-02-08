97c26c53765a062eb2c3b59f26fa00e163a5b2a6
Hegazian <38352684+Hegazian@users.noreply.github.com>
2022-02-08 22:26:04 +0200

    Delete Applications/7SEGMENT directory

--- Applications/7SEGMENT/Release/7Segment.d
+++
@@ -1,8 +0,0 @@
-7Segment.o 7Segment.o: ../7Segment.c ../LIB/Types.h ../DIO.h \
-  ../7Segment.h
-
-../LIB/Types.h:
-
-../DIO.h:
-
-../7Segment.h:
--- Applications/7SEGMENT/Release/main.d
+++
@@ -1,5 +0,0 @@
-main.o main.o: ../main.c ../LIB/Types.h ../7Segment.h
-
-../LIB/Types.h:
-
-../7Segment.h:
